0ab6d57a8ed5a1e9b93dcf147b1511022ff24b85
abdfnx <abdfnabd@gmail.com>
2022-08-23 17:47:45 +0300

    remove language checker

--- main.go
+++ main.go
@@ -18,45 +18,31 @@ func GetBotInfo(value string) string {
 }
 
 func GetToken() string {
-	if GetBotInfo("bot.lang") != "go" {
-		panic(errors.New("ERROR: Your Bot language is not Go"))
-	} else {
-		data := gjson.Get(string(constants.BotwayConfig), "botway.bots."+GetBotInfo("bot.name")+".bot_token").String()
+	data := gjson.Get(string(constants.BotwayConfig), "botway.bots."+GetBotInfo("bot.name")+".bot_token").String()
 
-		return data
-	}
+	return data
 }
 
 func GetAppId() string {
-	if GetBotInfo("bot.lang") != "go" {
-		panic(errors.New("ERROR: Your Bot language is not Go"))
-	} else {
-		id := "bot_app_id"
+	id := "bot_app_id"
 
-		if GetBotInfo("bot.type") == "slack" {
-			id = "bot_app_token"
-		}
-
-		data := gjson.Get(string(constants.BotwayConfig), "botway.bots."+GetBotInfo("bot.name")+"."+id).String()
-
-		return data
+	if GetBotInfo("bot.type") == "slack" {
+		id = "bot_app_token"
 	}
+
+	data := gjson.Get(string(constants.BotwayConfig), "botway.bots."+GetBotInfo("bot.name")+"."+id).String()
+
+	return data
 }
 
 func GetSigningSecret() string {
-	if GetBotInfo("bot.lang") != "go" {
-		panic(errors.New("ERROR: Your Bot language is not Go"))
-	} else {
-		data := gjson.Get(string(constants.BotwayConfig), "botway.bots."+GetBotInfo("bot.name")+".signing_secret").String()
+	data := gjson.Get(string(constants.BotwayConfig), "botway.bots."+GetBotInfo("bot.name")+".signing_secret").String()
 
-		return data
-	}
+	return data
 }
 
 func GetGuildId(serverName string) string {
-	if GetBotInfo("bot.lang") != "go" {
-		panic(errors.New("ERROR: Your Bot language is not Go"))
-	} else if GetBotInfo("bot.type") != "discord" {
+	if GetBotInfo("bot.type") != "discord" {
 		panic(errors.New("ERROR: This function/feature is only working with discord bots"))
 	} else {
 		data := gjson.Get(string(constants.BotwayConfig), "botway.bots."+GetBotInfo("bot.name")+".guilds."+serverName+".server_id").String()
